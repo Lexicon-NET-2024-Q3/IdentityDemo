aceae389391551bcdb0f9a89e5d89a0005fa36a8
Jonathan Krall <jonathan.krall@lexicon.se>
2024-11-12 11:53:44 +0100

    Added ApplicationUser

--- IdentityDemo/Data/Migrations/20241112104741_UpdatedUserTable.Designer.cs
+++ IdentityDemo/Data/Migrations/20241112104741_UpdatedUserTable.Designer.cs
@@ -4,6 +4,7 @@ using IdentityDemo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IdentityDemo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241112104741_UpdatedUserTable")]
+    partial class UpdatedUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
